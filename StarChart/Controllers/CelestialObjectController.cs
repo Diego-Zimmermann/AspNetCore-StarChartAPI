83124cc89810f587df9779353e30b4051db96a5d
Diego-Zimmermann <95628718+Diego-Zimmermann@users.noreply.github.com>
2022-11-14 12:33:24 -0300

    ba

---
+++ StarChart/Controllers/CelestialObjectController.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using StarChart.Data;
+
+namespace StarChart.Controllers
+{
+    public class CelestialObjectController : Controller
+    {
+
+        private readonly ApplicationDbContext _context;
+
+        public CelestialObjectController(ApplicationDbContext context)
+        {
+            _context = context;
+        }
+
+        [Route("")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
